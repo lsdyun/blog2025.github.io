62ae098845092d4bcce247b2d458da98d6bbf728
lsdyun <2307526822@qq.com>
2025-05-29 18:09:57 +0800

    Python代码

---
+++ python/first/hello.py
@@ -0,0 +1,120 @@
+""" print("hello world!")
+print("你好，世界！") """
+
+""" 
+str = 'helloworld'
+print(str[2:5])
+print(str[0:-1]) 
+print(str[1:5:2])
+print("this is a line with \n yourname")
+print(r"this is a line with \n yourname") 
+"""
+
+#input("\n\n按下 enter 键后退出。")
+#import sys; x = 'hello'; sys.stdout.write(x + '\n')
+""" 
+print("hello", end="")
+print("world!") 
+"""
+
+""" 
+a = 10
+b = "hello"
+print(type(a))
+
+print(type(b)) 
+"""
+
+""" 
+a = 100
+print(isinstance(a, int)) 
+"""
+
+""" 
+print(2 / 4)
+print(2 // 4)
+ """
+
+""" a = 10
+b = -90.
+print(a + b) """
+
+""" str = 'helloworld'
+print(str * 2) """
+
+""" str = 'helloworld'
+str[0] = 'g'
+print(str) """
+
+""" a = ['a','b']
+b = ['c','d','e']
+print(a + b)
+# 输出 ['a','b','c','d','e']
+print(a * 2)
+# 输出 ['a','b','a','b'] """
+
+""" list = ['a','b','c','d','e']
+print(list[1:3])
+print(list[1:4:2]) """
+
+""" a = ['a','b','']
+a[2] = 'c'
+print(a) """
+
+""" lst = [3, 1, 4]
+lst.reverse()
+print(lst) """
+
+""" tuple = ('a', 10, 33, 'hello',3,5,1)
+print(tuple)
+print(tuple[1:3])
+print(tuple * 2) 
+print(tuple[1:6:2]) """
+
+""" tuple = ('a', 10)
+tuple[0] = 1
+print(tuple)
+ """
+
+""" tupleA = ('a')
+print(type(tupleA))
+tupleB = ('a',)
+print(type(tupleB)) """
+
+""" a = {'value1', 'value2', 'value3', '...'}
+set(a)
+print(a) """
+
+""" a = {1, 2, 3, 4, 2, 2}
+print(a) """
+
+""" a = {1, 2, 3, 4, 2, 2}
+b = {'value1', 'value2', 3}
+print(a - b)
+print(a | b)
+print(a & b)
+print(a ^ b) """
+
+
+""" dictionary = {}
+dictionary[1] = "hello"
+print(dictionary[1]) """
+
+""" dicA = {"firstname": "zhang", "lastname": "san", "class": 1}
+print(dicA.keys())
+print(dicA.values()) """
+
+""" A = dict([('apple','red'), ('book',10), ('name', '张三')])
+print(A) """
+
+""" B = dict(apple='red', book=10, name='张三')
+print(B) """
+
+""" A = [('apple','red'), ('book',10), ('name', '张三')]
+print(dict(A)) """
+
+""" B = {'apple': 'red', 'book': 10, 'name': '张三'}
+print(B) """
+
+x = b"hello"
+print(type(x))
