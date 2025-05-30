affc271a6dff3d27792150f562162331b2c9881f
lsdyun <2307526822@qq.com>
2025-05-30 12:25:55 +0800

    python学习代码第二次修改

--- python/first/hello.py
+++ python/first/hello.py
@@ -116,5 +116,91 @@ print(dict(A)) """
 """ B = {'apple': 'red', 'book': 10, 'name': '张三'}
 print(B) """
 
-x = b"hello"
-print(type(x))
+""" x = b"hello"
+print(type(x)) """
+
+""" name = '张三'
+print('Hello %s' % name) """
+
+""" name = '张三'
+print(f'Hello {name}') """
+
+""" print(f'{1+1}') """
+
+""" a = {'name': '张三','word': '你好'}
+b = f'{a["name"]}: {a["word"]}'
+print(b) """
+
+""" lst = [1, 'a', 2, 3, 4, 5]
+del lst[1]
+print(lst) """
+
+""" a = ['a', 'b', 'c']
+n = [1, 2, 3]
+x = [a, n]
+print(x)
+print(x[0])
+print(x[0][1])
+print(x[1][1]) """
+
+""" import operator
+
+a = [1, 2]
+b = [2, 3]
+c = [2, 3]
+print("a和b是否相同: ", operator.eq(a,b))
+print("c和b是否相同: ", operator.eq(c,b)) """
+
+"""
+tuple = ('a', 10, 33, 'hello')
+
+del tuple[3]
+print(tuple) 
+del tuple
+print(tuple)"""
+
+""" list1= ['a','b','c','d','e']
+tuple1=tuple(list1)
+print(tuple1) """
+
+""" dicA = {"firstname": "zhang", "lastname": "san", "class": 1}
+
+print(dicA['firstname'])
+print(dicA['class'])
+dicA["class"] = 10
+dicA["firstname"] = '张'
+print(dicA) """
+
+""" dicA = {"firstname": "zhang", "lastname": "san", "class": 1}
+del dicA['lastname']
+dicA.clear()
+del dicA
+print(dicA) """
+
+""" a = {1, 2, 3, 4, 2, 2}
+a.add(10)
+print(a) """
+
+""" a = {1, 2, 3, 4, 2, 2}
+a.update(["列表","100"]) # 元组
+a.update({111,333}) # 集合
+a.update({"firstname": "张", "lastname": "三"})
+a.update((100,222))
+print(a)  """
+
+""" a = {1, 2, 3, 4, "haha"}
+a.remove("haha")
+a.remove('aaa')
+print(a) """
+
+""" a = {1, 2, 3, 4, "haha"}
+a.discard('aaa')
+print(a) """
+
+""" a = {1, 2, 3, 4, "haha"}
+a.pop()
+print(a) """
+
+a = {1, 2, 3, 4, "haha"}
+a.clear()
+print(a)
